aec370d4c08c3a14c9cd683afb5566893b7d2d8c
mushihata <mushihata@gmail.com>
2024-02-14 21:32:20 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let hero = document.querySelector('.hero');
+
+function phones(phone){
+    img.src = phone;
+}
+
+function colors(color){
+    hero.style.background = color;
+}
